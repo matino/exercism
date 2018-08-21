d2186d0070e40b9a6bb2a8103e377f5ccfc6e7a5
Mateusz Czubak <mateusz.c@webinterpret.com>
2018-08-21 12:23:08 +0200

    Add protein translation solution

---
+++ elixir/protein-translation/protein_translation.exs
@@ -0,0 +1,66 @@
+defmodule ProteinTranslation do
+
+  @proteins %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP",
+  }
+
+  @codons @proteins |> Map.keys()
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    proteins = rna
+      |> String.split(~r/.{3}/, include_captures: true, trim: true)
+      |> Enum.map(fn codon -> @proteins[codon] end)
+      |> Enum.take_while(fn protein -> protein != "STOP" end)
+    if nil in proteins do
+      {:error, "invalid RNA"}
+    else
+      {:ok, proteins}
+    end
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) when codon in @codons, do: {:ok, @proteins[codon]}
+  def of_codon(_), do: {:error, "invalid codon"}
+
+end
